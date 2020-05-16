adab95e2e50386158e20eb75372d2aeb77978eb3
Rohit <rohitkumar377024@gmail.com>
2020-05-16 11:53:37 +0530

    Added: Todo - Basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Checkbox } from '@material-ui/core';
 
 function App() {
-  return <h1>Hello :)</h1>;
+  return (
+    <div className="todo-container">
+      <input className="todo-checkbox" type="checkbox" />
+      <div className="todo-info">
+        <p className="todo-text">Buy Milk</p>
+        <p className="todo-time">Mon, Apr 30</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
