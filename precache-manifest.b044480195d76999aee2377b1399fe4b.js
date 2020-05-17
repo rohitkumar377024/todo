a0b22b199e583a640d857b23a0869e03228175ac
Rohit <rohitkumar377024@gmail.com>
2020-05-17 09:09:58 +0530

    Updates

---
+++ precache-manifest.b044480195d76999aee2377b1399fe4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2713d4baed4ba217d8deaa0d6120ca2d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "3db784afeb45b2ea5287",
+    "url": "/todo/static/js/2.984bc027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.984bc027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b85266c43165fe8a40",
+    "url": "/todo/static/js/main.4defeca9.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  }
+]);
